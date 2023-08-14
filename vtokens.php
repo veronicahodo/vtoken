fafdb90c26b74c4e9ac4a8c058921e34539276f2
Veronica <veronica@veronicahodo.com>
2023-08-14 01:01:49 -0700

    Update vtokens.php

--- vtokens.php
+++ vtokens.php
@@ -9,52 +9,58 @@
 
 require_once('vcrud.php');
 
-class VTOKEN {
+class VTOKEN
+{
   private $token;  // Magic token itself
   private $userId;  // userId of the token owner
   private $expiration;  // when the token is no longer valid
   private $timeLimit;  // how long in the future to set expiration
-  
-  
-  function __construct($timeLimit = 30000) {
+
+
+  function __construct($timeLimit = 30000)
+  {
     $this->token = bin2hex(random_bytes(32));
     $this->userId = 0;
     $this->expiration = '';
   }
 
-  function generateToken($userId, VCRUD $c) {
+  function generateToken($userId, VCRUD $c)
+  {
     // This is what we call 
     $this->userId = $userId;
-    $this->expiration = date('YmdHis',strtotime("+".$this->timeLimit." seconds",strtotime(date("Y-m-d H:i:s")));
+    $this->expiration = date('YmdHis', strtotime("+" . $this->timeLimit . " seconds", strtotime(date("Y-m-d H:i:s"))));
     $this->token = bin2hex(random_bytes(32));
 
-    $c->delete('tokens',[
-               ['userId','=',$userId],
-               ['expiration','<',date('YmdHis')]
-               ]);
-    $c->create('tokens',[
-                 'userId' => $this->userId,
-                 'expiration' => $this->expiration,
-                 'token' => $this->token
-               ]);
+    $c->delete('tokens', [
+      ['userId', '=', $userId],
+      ['expiration', '<', date('YmdHis')]
+    ]);
+    $c->create('tokens', [
+      'userId' => $this->userId,
+      'expiration' => $this->expiration,
+      'token' => $this->token
+    ]);
     return $this->token;
   }
 
-  function flushExpired($userId, VCRUD $c) {
-    $c->delete('tokens',[
-               ['userId','=',$userId],
-               ['expiration','<',date('YmdHis')]
-               ];
+  function flushExpired($userId, VCRUD $c)
+  {
+    $c->delete('tokens', [
+      ['userId', '=', $userId],
+      ['expiration', '<', date('YmdHis')]
+    ]);
   }
 
 
-  function flushAll($userId,VCRUD $c) {
-    $c->delete('tokens',[['userId','=',$userId]]);
+  function flushAll($userId, VCRUD $c)
+  {
+    $c->delete('tokens', [['userId', '=', $userId]]);
   }
 
 
-  function getUserIdFromToken($token, VCRUD $c) {
-    if ($user = $c->read('tokens',[['token','=',$token]])) {
+  function getUserIdFromToken($token, VCRUD $c)
+  {
+    if ($user = $c->read('tokens', [['token', '=', $token]])) {
       if ($user[0]['expiration'] > date('YmdHis')) {
         return $user[0]['userId'];
       } else {
